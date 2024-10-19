1fd86ee4c2266d4de8c1bf08f3393ca4e405b1a6
Ismael Santana <souz4ismael@gmail.com>
2024-10-19 12:41:53 -0300

    register leave parking slot command handler

--- src/modules/parking-slots/commands/index.ts
+++ src/modules/parking-slots/commands/index.ts
@@ -1,3 +1,7 @@
+import { LeaveParkingSlotCommandHandler } from './leave-parking-slot/leave-parking-slot-command-handler';
 import { ReserveParkingSlotCommandHandler } from './reserve-parking-slot/reserve-parking-slot-command-handler';
 
-export const UseCasesHandlers = [ReserveParkingSlotCommandHandler];
+export const UseCasesHandlers = [
+  ReserveParkingSlotCommandHandler,
+  LeaveParkingSlotCommandHandler,
+];
